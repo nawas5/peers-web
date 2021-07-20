8c24b5cacf0461e10e626328a0d8ce01894e8689
nawas5 <natasha.masalckova@yandex.ru>
2021-07-20 16:04:51 +0300

    Added chart web

---
+++ open-log.py
@@ -0,0 +1,32 @@
+import csv
+import pandas as pd
+
+filename = "data/01072021105028peers.log"
+filename_csv = "data/01072021105028peers-log.csv"
+
+with open(filename_csv, 'w', encoding='utf-8') as w_file:
+    file_writer = csv.writer(w_file, delimiter=",", lineterminator="\r")
+    file_writer.writerow(["time","name","x","y","z"])
+    with open(filename, 'r') as file:
+        for line in file:
+            if line.find("CLE: TAG") > 0:
+                a = line.split()
+                # проверка на корректность точек
+                if a[4] == "1":
+                    time = a[0]
+                    name = a[3]
+                    x = float(a[5])
+                    y = float(a[6])
+                    z = float(a[7])
+                    file_writer.writerow([str(time), str(name), str(x), str(y), str(z)])
+
+
+df = pd.read_csv(filename_csv, error_bad_lines=False)
+df["number"] = 0
+mname = df["name"].unique()
+for i in range(len(df)):
+    for j in range(len(mname)):
+        if df["name"][i] == mname[j]:
+            df["number"][i] = j+1
+
+df.to_csv("data/log.csv")
